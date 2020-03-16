d26b38798f8b4a57914d0946181044d9b37d05cb
Swedish <tarthurf@yahoo.com>
2020-03-16 16:30:06 -0700

    added core files to project, installed required modules, built sql database file

---
+++ employeeDBconnect.sql
@@ -0,0 +1,34 @@
+-- for windows users with SQLworkbench 8
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+
+DROP DATABASE IF EXISTS employeesDB;
+
+CREATE database employeesDB;
+
+USE employeesDB;
+
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
+);
+
+SELECT * FROM department;
+
+CREATE TABLE employee_role (
+  id INT PRIMARY KEY,
+  title VARCHAR(130) NOT NULL,
+  salary DECIMAL(10,2) NULL,
+  department_id INT NOT NULL
+);
+
+SELECT * FROM employee_role;
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL
+);
+
+SELECT * FROM employee;
