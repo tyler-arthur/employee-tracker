51e98fa37cadc1731e4e1bff1276ce24ca6be39a
Swedish <tarthurf@yahoo.com>
2020-03-16 19:30:46 -0700

    Uopdated sql schema and seeds.sql file

--- seeds.sql
+++ seeds.sql
@@ -3,34 +3,82 @@ USE employeesDB;
 
 /* !!!CHANGE COLUMN NAMES IF DIFFERENT!!! */
 INSERT INTO department (id, department_name)
-VALUES (01, "Intructors");
+VALUES (900, "Intructors");
 
 INSERT INTO department (id, department_name)
-VALUES (02, "TA's");
+VALUES (800, "TA's");
 
 INSERT INTO department (id, department_name)
-VALUES (03, "Students");
+VALUES (100, "Students");
 
 
 
 /* !!!CHANGE COLUMN NAMES IF DIFFERENT!!! */
 INSERT INTO employee_role (id, title, salary, department_id)
-VALUES ( 10,"Instructor", 100000.00, 01);
+VALUES ( 910,"Instructor", 100000.00, 900);
 
 INSERT INTO employee_role (id, title, salary, department_id)
-VALUES ( 20,"TA", 90000.00, 02);
+VALUES ( 810,"TA", 90000.00, 800);
 
 INSERT INTO employee_role (id, title, salary, department_id)
-VALUES ( 30,"Student", 0.00, 03);
+VALUES ( 110,"Student", 0.00, 100);
 
 
 
 /* !!!CHANGE COLUMN NAMES IF DIFFERENT!!! */
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (123, "Harrison", "Schaen", 01);
+VALUES (911, "Harrison", "Schaen", 910, NULL);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (456, "Steph", "Huynh", 02, 01);
+VALUES (811, "Steph", "Huynh", 810, 911);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (789, "Steven", "Landgraf", 02, 01);
+VALUES (812, "Steven", "Landgraf", 810, 911);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (111, "Daniel", "Angulo", 110, 811);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (112, "Said", "Aguilar", 110, 811);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (113, "Matthew", "Bell", 110, 811);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (114, "Julian", "Acosta", 110, 811);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (115, "Tyler", "Arthur", 110, 811);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (116, "Javier", "Banuelos", 110, 811);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (117, "Art", "Aragon", 110, 811);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (118, "Cara", "Bunnell", 110, 811);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (119, "Jaycee", "Bagtas", 110, 812);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (120, "Alexia", "Chalita", 110, 812);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (121, "Lizbeth", "Glasser", 110, 812);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (122, "Collin", "Kier", 110, 812);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (123, "Brian", "Monteverde", 110, 812);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (124, "Greg", "Schuman", 110, 812);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (125, "Amy", "Fabella", 110, 812);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (126, "Jason", "Riley", 110, 812);
